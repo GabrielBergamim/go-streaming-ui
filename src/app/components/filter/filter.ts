5f40fde74908f80a7b76279f13379e628810745d
Gabriel Bergamim <gabriel.bergamim@outlook.com>
2025-07-28 15:57:16 -0300

    feat: add roboto font and primeng components for filter component

--- src/app/components/filter/filter.ts
+++ src/app/components/filter/filter.ts
@@ -1,11 +1,19 @@
 import { Component, EventEmitter, model, Output} from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { InputTextModule } from "primeng/inputtext";
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.html',
   styleUrl: './filter.scss',
-  imports: [FormsModule]
+  imports: [
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule
+  ]
 })
 export class FilterComponent {
 
